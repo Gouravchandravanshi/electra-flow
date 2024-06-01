8f958d093b54fbed9d4ee5ff58c3ef344f642b2a
Gouravchandravanshi <2021et28go@mitsgwl.ac.in>
2024-06-01 13:36:14 +0530

    form updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -104,6 +104,54 @@ const Contact = () => {
             </div>
           </div>
      </section>
+     <div class="contactus-form-container">
+     <form class="form" action="https://formcarry.com/s/ZTaPd7_0Dmm" method="POST">
+
+              <div class="formfield-container">
+                <input
+                  class="formfield"
+                  type="text"
+                  name="name"
+                  id=""
+                  placeholder="Enter your name "
+                />
+
+                <input
+                  class="formfield"
+                  type="email"
+                  name="email"
+                  id=""
+                  placeholder="Enter your mail address"
+                />
+
+                <input
+                  class="formfield"
+                  type="text"
+                  name="subject"
+                  id=""
+                  placeholder="Enter your subject"
+                />
+
+                <textarea
+                  name="message"
+                  id=""
+                  cols="30"
+                  rows="10"
+                  class="formfield formfield-textarea"
+                  placeholder="Enter your message"
+                ></textarea>
+              </div>
+
+              <div>
+                <button type="submit" class="btn-pink" id="submit-btn">
+                  Send Message<i
+                    class="submit-icon fa-solid fa-paper-plane"
+                  ></i>
+                </button>
+              </div>
+            </form>
+            </div>
+
     </div>
   )
 }
