f8856689334ca92ac839ae2313f0ec4892b0d3b6
Gouravchandravanshi <2021et28go@mitsgwl.ac.in>
2024-06-15 15:20:11 +0530

    cards added

--- src/components/assets/dcchargerdata.js
+++ src/components/assets/dcchargerdata.js
@@ -1,33 +1,33 @@
 export const DcChargerData = [
     {
         
-        "image": "https://www.servotech.in/_next/image?url=%2Fimages%2Fev%2FDC-charger-20kW.webp&w=384&q=75",
-        "heading":"DC Charging Station 15kW | 20kW",
+        "image": "https://image.made-in-china.com/2f0j00iozldsyhlMfV/60kw-180kw-Electric-Vehicle-Charging-Piles-EV-DC-Charger.webp",
+        "heading":"DC Charging Station 60 kW single Gun",
         "description": "Charging Gun as per CCS 2 Standard. 1 Output for Charging PortInput Voltage- 3 PhaseUserAuthentication- RFID/ QR Code Scan/ OCPP 1.6 J Network Connection- 4G Module/Wifi/ Ethernet"
     },
     {
         
-        "image": "https://www.servotech.in/_next/image?url=%2Fimages%2Fev%2FDC-charger-30kW.webp&w=384&q=100",
-        "heading":"CCS-2/ Charging Station - Single Gun : 30kW",
-        "description": "Charging Gun as per CCS 2 Standard.1 Output for Charging PortInput Voltage- 3 PhaseUser Authentication- RFID/ QR Code Scan/ OCPP 1.6 J Connectivity- GSM / Ethernet / WiFi"
+        "image": "https://www.kinouwellparking.com/wp-content/uploads/2022/12/1-0.jpg",
+        "heading":"60/120 kW Dual Gun Charger",
+        "description": "Charging Gun as per CCS 2 Standard.2 Output for Charging PortInput Voltage- 3 PhaseUser Authentication- RFID/ QR Code Scan/ OCPP 1.6 J Connectivity- GSM / Ethernet / WiFi"
     },
-    {
+    // {
         
-        "image": "https://www.servotech.in/_next/image?url=%2Fimages%2Fev%2FDC-charger-30kW-2.webp&w=384&q=75",
-        "heading":"CCS-2/ Charging Station - Single Gun : 30kW",
-        "description": "Charging Gun as per CCS 2 Standard. 1-2 Output for Charging Port Input Voltage- 3 Phase User Authentication- RFID/ QR Code Scan/ OCPP 1.6 J Connectivity- GSM / Ethernet / WiFi"
-    },
-    {
+    //     "image": "https://www.servotech.in/_next/image?url=%2Fimages%2Fev%2FDC-charger-30kW-2.webp&w=384&q=75",
+    //     "heading":"CCS-2/ Charging Station - Single Gun : 30kW",
+    //     "description": "Charging Gun as per CCS 2 Standard. 1-2 Output for Charging Port Input Voltage- 3 Phase User Authentication- RFID/ QR Code Scan/ OCPP 1.6 J Connectivity- GSM / Ethernet / WiFi"
+    // },
+    // {
         
-        "image": "https://www.servotech.in/_next/image?url=%2Fimages%2Fev%2FDC-charger-30kW-3.webp&w=384&q=75",
-        "heading":"CCS-2/ Charging Station - Single Gun : 30kW",
-        "description": "Charging Gun as per CCS 2 Standard. 1-2 Output for Charging PortInput Voltage- 3 PhaseUser Authentication- RFID/ QR Code Scan/ OCPP 1.6 JConnectivity- GSM / Ethernet / WiFi"
-    },
-    {
+    //     "image": "https://www.servotech.in/_next/image?url=%2Fimages%2Fev%2FDC-charger-30kW-3.webp&w=384&q=75",
+    //     "heading":"CCS-2/ Charging Station - Single Gun : 30kW",
+    //     "description": "Charging Gun as per CCS 2 Standard. 1-2 Output for Charging PortInput Voltage- 3 PhaseUser Authentication- RFID/ QR Code Scan/ OCPP 1.6 JConnectivity- GSM / Ethernet / WiFi"
+    // },
+    // {
         
-        "image": "https://www.servotech.in/_next/image?url=https%3A%2F%2Fwww.servotech.in%2Fblog%2Fwp-content%2Fuploads%2F2024%2F04%2FDC-60kW-Inhouse-ver-2-1.png&w=384&q=75",
-        "heading":"CCS-2/ Charging Station - Dual Gun: 60kW",
-        "description": "Charging Gun as per CCS 2 Standard.2 Output for Charging PortInput Voltage- 3 PhaseUse Authentication- RFID/ QR Code Scan/ OCPP 1.6 JConnectivity- GSM / Ethernet / WiFi"
-    },
+    //     "image": "https://www.servotech.in/_next/image?url=https%3A%2F%2Fwww.servotech.in%2Fblog%2Fwp-content%2Fuploads%2F2024%2F04%2FDC-60kW-Inhouse-ver-2-1.png&w=384&q=75",
+    //     "heading":"CCS-2/ Charging Station - Dual Gun: 60kW",
+    //     "description": "Charging Gun as per CCS 2 Standard.2 Output for Charging PortInput Voltage- 3 PhaseUse Authentication- RFID/ QR Code Scan/ OCPP 1.6 JConnectivity- GSM / Ethernet / WiFi"
+    // },
 ]
  export default DcChargerData
--- src/components/assets/ractifier.js
+++ src/components/assets/ractifier.js
@@ -1,34 +1,34 @@
 const ractifierData = [
     {
-            "image": "",
-            "heading":"7.7kw charger",
-            "description": "This is description"
+            "image": "https://chargedevs.com/wp-content/uploads/2023/10/Screenshot-2023-10-20-at-11.31.25-AM-copy.jpg",
+            "heading":"30 kW ractifier",
+            "description": "OmniOn Power has developed new 30-kilowatt power supplies specifically designed for the EV market. The EV100H3NK and EV101H3N1K rectifiers convert commercial AC power to DC power for EV fast charging stations"
     },
-    {
-            "image": "",
-            "heading":"7.7kw charger",
-            "description": "This is description"
-    },
-    {
-            "image": "",
-            "heading":"7.7kw charger",
-            "description": "This is description"
-    },
-    {
+//     {
+//             "image": "",
+//             "heading":"7.7kw charger",
+//             "description": "This is description"
+//     },
+//     {
+//             "image": "",
+//             "heading":"7.7kw charger",
+//             "description": "This is description"
+//     },
+//     {
         
         
-            "image": "",
-            "heading":"7.7kw charger",
-            "description": "This is description"
+//             "image": "",
+//             "heading":"7.7kw charger",
+//             "description": "This is description"
         
-    },
-    {
+//     },
+//     {
         
         
-            "image": "",
-            "heading":"7.7kw charger",
-            "description": "This is description"
+//             "image": "",
+//             "heading":"7.7kw charger",
+//             "description": "This is description"
         
-    },
+//     },
 ]
 export default ractifierData
